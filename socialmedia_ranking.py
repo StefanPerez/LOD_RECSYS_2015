44e1b36d83a0196a21a09b2b53d2328f0d7df260
sudoStefan <stefan.perez@gmail.com>
2015-04-17 16:13:51 +0200

    Ranking based on Twitter
    Programmes Ranked based on Twitter API

--- socialmedia_ranking.py
+++ socialmedia_ranking.py
@@ -1,61 +1,59 @@
 import twitter
 import json
 
-# XXX: Go to http://dev.twitter.com/apps/new to create an app and get values
-# for these credentials, which you'll need to provide in place of these
-# empty string values that are defined as placeholders.
-# See https://dev.twitter.com/docs/auth/oauth for more information
-# on Twitter's OAuth implementation.
+def init_twitter(get_input):
+    CONSUMER_KEY = 'p7HUdPLlkKaqlPn6TzKkA'
+    CONSUMER_SECRET = 'R7I1LRuLY27EKjzulutov74lKB0FjqcI2DYRUmsu7DQ'
+    OAUTH_TOKEN = '14898655-TE9dXQLrzrNd0Zwf4zhK7koR5Ahqt40Ftt35Y2qY'
+    OAUTH_TOKEN_SECRET = 'q1lSRDOguxQrfgeWWSJgnMHsO67bqTd5dTElBsyTM'
 
-CONSUMER_KEY = 'p7HUdPLlkKaqlPn6TzKkA'
-CONSUMER_SECRET = 'R7I1LRuLY27EKjzulutov74lKB0FjqcI2DYRUmsu7DQ'
-OAUTH_TOKEN = '14898655-TE9dXQLrzrNd0Zwf4zhK7koR5Ahqt40Ftt35Y2qY'
-OAUTH_TOKEN_SECRET = 'q1lSRDOguxQrfgeWWSJgnMHsO67bqTd5dTElBsyTM'
-
-auth = twitter.oauth.OAuth(OAUTH_TOKEN, OAUTH_TOKEN_SECRET,
+    auth = twitter.oauth.OAuth(OAUTH_TOKEN, OAUTH_TOKEN_SECRET,
                            CONSUMER_KEY, CONSUMER_SECRET)
 
-twitter_api = twitter.Twitter(auth=auth)
+    twitter_api = twitter.Twitter(auth=auth)
 
-q = '#TOP GEAR #BBC'
+    get_query_input = get_input
 
-count = 100
+    query_twitter(twitter_api, get_query_input)
+    return
 
-# See https://dev.twitter.com/docs/api/1.1/get/search/tweets
+def query_twitter(get_auth,get_query_input):
 
-search_results = twitter_api.search.tweets(q=q, count=count)
+    q = get_query_input
 
-statuses = search_results['statuses']
+    count = 100
 
+    # See https://dev.twitter.com/docs/api/1.1/get/search/tweets
 
-# Iterate through 5 more batches of results by following the cursor
+    search_results = get_auth.search.tweets(q=q, count=count)
 
-for _ in range(5):
-    print("Length of statuses", len(statuses))
-    try:
-        next_results = search_results['search_metadata']['next_results']
-        # Create a dictionary from next_results, which has the following form:
-        # ?max_id=313519052523986943&q=NCAA&include_entities=1
-        kwargs = dict([ kv.split('=') for kv in next_results[1:].split("&") ])
-        search_results = twitter_api.search.tweets(**kwargs)
-        statuses += search_results['statuses']
-    except KeyError:
-        pass
-    # except KeyError, e:
-    #     pass
-
-twitter_result = []
-
-# Show one sample search result by slicing the list...
-#print json.dumps(statuses[0]['created_at'], statuses[0]['created_at'], indent=1)
-
-status_texts = [ status['text']
-                 for status in statuses ]
-
-status_created = [ status['created_at']
-                 for status in statuses ]
+    statuses = search_results['statuses']
 
 
-# Explore the first 5 items for each...
+    # Iterate through 5 more batches of results by following the cursor
 
-print(json.dumps(status_texts[0:200] + status_created[0:200], indent=1))
+    for _ in range(5):
+        print("Length of statuses", len(statuses))
+        try:
+            next_results = search_results['search_metadata']['next_results']
+            # Create a dictionary from next_results, which has the following form:
+            # ?max_id=313519052523986943&q=NCAA&include_entities=1
+            kwargs = dict([ kv.split('=') for kv in next_results[1:].split("&") ])
+            search_results = twitter_api.search.tweets(**kwargs)
+            statuses += search_results['statuses']
+        except KeyError:
+            pass
+        # except KeyError, e:
+        #     pass
+
+    status_texts = [ status['text']
+                     for status in statuses ]
+
+    status_created = [ status['created_at']
+                     for status in statuses ]
+
+    print(json.dumps(status_texts[0:200] + status_created[0:200], indent=1))
+
+    return
+
+init_twitter('#TOP GEAR #BBC')
